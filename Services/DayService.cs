9ec103757d0a45c51ec151157f3494da04621b4f
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-09-26 10:44:20 +0100

    Add files via upload

---
+++ Services/DayService.cs
@@ -0,0 +1,27 @@
+﻿using DOA.Data;
+using DOA.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DOA.Services
+{
+    public class DayService : IDayService
+    {
+
+        private readonly ApplicationDbContext _db;
+        public DayService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Day[]> GetIncompleteDaysAsync()
+        {
+            var myDays = await _db.AllDaysFromDb.ToArrayAsync();
+            return myDays;
+        }
+
+    }
+}
